d95bd06e4b6620afe96a3c57c7054fcedb2aa33c
mfilmer <mattfilmer@gmail.com>
2012-09-20 15:29:40 -0400

    Days in Month and kol seq

--- kolakoski/golfed.hs
+++ kolakoski/golfed.hs
@@ -1,5 +1,3 @@
 import System
-main=do
- args<-getArgs
- let a=1:2:drop 2(concat.zipWith replicate a.cycle$[1,2])
- putStrLn$concatMap(\b->' ':show b)$take(read(args!!0)::Int)a
+a=1:2:drop 2(concat.zipWith replicate a.cycle$[1,2])
+main=do args<-getArgs;putStr$concatMap((' ':).show)$take(read$args!!0::Int)a
